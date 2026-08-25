fc4dfd13eea211a1ec88dcdb0a62c44d2a8f8034
haideralbaba-rgb <haideralbaba130@gmail.com>
2026-08-25 10:07:17 +0300

    fix: use Vite Nitro deployment pipeline for TanStack Start

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
-    tanstackStart(),
     tailwindcss(),
+    tanstackStart(),
+    nitro(),
     react(),
   ],
 })
